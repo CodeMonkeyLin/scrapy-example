df856cfcf7e49a410f52e29fef3ad854cb20acd3
gaodonglin <gaodonglin@zeaho.com>
2020-03-25 11:26:26 +0800

    Add project

---
+++ tutorial/readme.md
@@ -0,0 +1,11 @@
+## 环境 
+python3
+### 安装scrapy
+```bash
+pip install scrapy
+```
+## 运行
+```bash
+cd tutorial
+scrapy crawl example
+```
---
+++ tutorial/spiders/example.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+    allowed_domains = ['baidu.com']
+    start_urls = [
+        'https://www.zeaho.com/'
+    ]
+
+    def parse(self, response):
+        print('---------')
+        print(response.xpath(
+            '//li[@class="dropdown"]/a/text()').get(default='not-found'))
